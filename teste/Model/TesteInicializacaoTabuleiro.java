efb9beb2a944c772232780d94dee582eee13a9d3
LuizAugustoPapa <luiz.papa@aluno.puc-rio.br>
2023-10-22 01:47:26 -0300

    Mudança pasta dos testes

--- teste/Model/TesteInicializacaoTabuleiro.java
+++ teste/Model/TesteInicializacaoTabuleiro.java
@@ -6,22 +6,21 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class TesteInicializacaoTabuleiro {
-	private Jogador jogador;
 
 	@Before
 	public void setup() {
-		jogador = Jogador.CriaJogs("cor1","jogador1");
-		jogador = Jogador.CriaJogs("cor2","jogador2");
-		jogador = Jogador.CriaJogs("cor3","jogador3");
-		jogador = Jogador.CriaJogs("cor4","jogador4");
-		jogador = Jogador.CriaJogs("cor5","jogador5");
-		jogador = Jogador.CriaJogs("cor6","jogador6");
+		Jogador.CriaJogs("cor1","jogador1");
+		Jogador.CriaJogs("cor2","jogador2");
+		Jogador.CriaJogs("cor3","jogador3");
+		Jogador.CriaJogs("cor4","jogador4");
+		Jogador.CriaJogs("cor5","jogador5");
+		Jogador.CriaJogs("cor6","jogador6");
 	}
 
 	@Test
 	public void testeInsereSetimoJogador() {
-		jogador = Jogador.CriaJogs("cor1","jogador7");
-		assertTrue(jogador.qtdeJogs() == 7);
+		Jogador.CriaJogs("cor1","jogador7");
+		assertTrue(Jogador.qtdeJogs() == 6);
 	}
 
 	@Test
