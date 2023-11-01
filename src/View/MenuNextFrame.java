2478a97fe7546b721f73d56b6680ebc55b2f95fa
Miranda Wopps <127881339+MirandaWopps@users.noreply.github.com>
2023-11-01 12:55:03 -0300

    Texts boxes feitas e adquirição de valores funcinando. Falta a parte de exclusão das cores e verificação de cor repetida.

--- src/View/MenuNextFrame.java
+++ src/View/MenuNextFrame.java
@@ -1,4 +1,4 @@
-package View;
+package src.View;
 
 import java.awt.Color;
 import java.awt.Dimension;
@@ -14,6 +14,7 @@ import javax.swing.JTextField;
 public class MenuNextFrame extends JFrame implements ActionListener{
 
 	// declares an array of integers
+    private String jogadoresArray[] = new String[6];     // allocates memory for 6 Strings
     private String coresArray[] = new String[6];     // allocates memory for 6 Strings
     private String qtdJogadoresString;  // esta dando erro fazer o casting dentro do listener
     private int qtdJogadores;
@@ -26,6 +27,13 @@ public class MenuNextFrame extends JFrame implements ActionListener{
 	private JTextField textField4; // Declare textField as an instance variable
 	private JTextField textField5; // Declare textField as an instance variable
 	private JTextField textField6; // Declare textField as an instance variable
+	
+	private JTextField textFieldCor1; // Declare textField as an instance variable
+	private JTextField textFieldCor2; 
+	private JTextField textFieldCor3; 
+	private JTextField textFieldCor4; 
+	private JTextField textFieldCor5; 
+	private JTextField textFieldCor6; 
 	
 	MenuNextFrame(){
 		this.setTitle("Escolha de Jogadores");
@@ -62,6 +70,26 @@ public class MenuNextFrame extends JFrame implements ActionListener{
 		textField6 = new JTextField(); 
 		textField6.setPreferredSize(new Dimension(250,40));
 		
+		
+		
+		textFieldCor1 = new JTextField(); 
+		textFieldCor1.setPreferredSize(new Dimension(250,40));
+		
+		textFieldCor2 = new JTextField(); 
+		textFieldCor2.setPreferredSize(new Dimension(250,40));
+		
+		textFieldCor3 = new JTextField(); 
+		textFieldCor3.setPreferredSize(new Dimension(250,40));
+		
+		textFieldCor4 = new JTextField(); 
+		textFieldCor4.setPreferredSize(new Dimension(250,40));
+		
+		textFieldCor5 = new JTextField(); 
+		textFieldCor5.setPreferredSize(new Dimension(250,40));
+		
+		textFieldCor6 = new JTextField(); 
+		textFieldCor6.setPreferredSize(new Dimension(250,40));
+		
 		//textField.setFont(new Font("Consolas",Font.PLAIN,35);
 		//textField.setForeground(new Color(0x00FF00));
 		jogadoresField.setText("Quantidade Jogadores");
@@ -72,7 +100,14 @@ public class MenuNextFrame extends JFrame implements ActionListener{
 		textField5.setText("Nome Jogador 5");
 		textField6.setText("Nome Jogador 6");
 		
+		textFieldCor1.setText("Cor Jogador 1");
+		textFieldCor2.setText("Cor Jogador 2");
+		textFieldCor3.setText("Cor Jogador 3");
+		textFieldCor4.setText("Cor Jogador 4");
+		textFieldCor5.setText("Cor Jogador 5");
+		textFieldCor6.setText("Cor Jogador 6");
 		
+		// qtdJogadores e Nome dos jogadores
 		this.add(jogadoresField);
 		this.add(textField1);
 		this.add(textField2);
@@ -80,40 +115,58 @@ public class MenuNextFrame extends JFrame implements ActionListener{
 		this.add(textField4);
 		this.add(textField5);
 		this.add(textField6);
+		
+		this.add(textFieldCor1);
+		this.add(textFieldCor2);
+		this.add(textFieldCor3);
+		this.add(textFieldCor4);
+		this.add(textFieldCor5);
+		this.add(textFieldCor6);
+		
 		this.add(button);
 		
 		this.pack();
-		this.setSize(300,400);
+		this.setSize(300,675);
 		this.setVisible(true);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == button) {
-			qtdJogadoresString = jogadoresField.getText();
-			coresArray[0] = textField1.getText();  // Armazenando		
-			coresArray[1] = textField2.getText();
-			coresArray[2] = textField3.getText();
-			coresArray[3] = textField4.getText();		
-			coresArray[4] = textField5.getText();
-			coresArray[5] = textField6.getText();
+			qtdJogadores = Integer.parseInt( jogadoresField.getText()   );  // falta exception para qnd tiver texto
+			//qtdJogadoresString = jogadoresField.getText();
+			//qtdJogadores = Integer.parseInt(qtdJogadoresString)  ;
+			jogadoresArray[0] = textField1.getText();  // Armazenando		
+			jogadoresArray[1] = textField2.getText();
+			jogadoresArray[2] = textField3.getText();
+			jogadoresArray[3] = textField4.getText();		
+			jogadoresArray[4] = textField5.getText();
+			jogadoresArray[5] = textField6.getText();
+			
+			coresArray[0] = textFieldCor1.getText();  // Armazenando		
+			coresArray[1] = textFieldCor2.getText();
+			coresArray[2] = textFieldCor3.getText();
+			coresArray[3] = textFieldCor4.getText();		
+			coresArray[4] = textFieldCor5.getText();
+			coresArray[5] = textFieldCor6.getText();
+			
+			
+			
+			
+			System.out.println(qtdJogadores);
 			
 			
 			for (int i= 0 ; i<6 ;i++) {
-			       // Vermelho                           // Verde                         // Azul                   // Preto                      // Branco                // Amarelo
-			
-				//if ( (coresArray[i] =="Vermelho") || (coresArray[i] =="Verde") || (coresArray[i] =="Azul") || (coresArray[i] =="Preto") || (coresArray[i] =="Branco") || (coresArray[i] =="Amarelo") ){
-					System.out.println("Nome: " + coresArray[i]); //
-				}
-			
-			
-			//}
-			
-		}		
-
-		
-		
-		
+			       // Vermelho                           // Verde                         // Azul                   // Preto                      // Branco                // Amarelo		
+				if ( (coresArray[i] =="Vermelho") || (coresArray[i] =="Verde") || (coresArray[i] =="Azul") || (coresArray[i] =="Preto") || (coresArray[i] =="Branco") || (coresArray[i] =="Amarelo") ){
+					System.out.println("Nome: " + jogadoresArray[i]); //
+					System.out.println("Cor: " + coresArray[i]+"\n"); 		
+				}	else {
+					System.out.println("NAO INSERIU COR CORRETAMENTE");
+					break;
+				} // else
+			}// for
+		}				
 	}
 	
 	
