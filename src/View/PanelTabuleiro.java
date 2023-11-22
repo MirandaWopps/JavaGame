36e0a632e16392121cd3a7436fbc6ab90dad64b4
Miranda Wopps <127881339+MirandaWopps@users.noreply.github.com>
2023-11-22 09:49:08 -0300

    botao cartas

--- src/View/PanelTabuleiro.java
+++ src/View/PanelTabuleiro.java
@@ -84,7 +84,7 @@ public class PanelTabuleiro extends JPanel implements Observer {
             } 
         });
 
-		// Botão posicionamento de tropas
+		// BotÃ£o posicionamento de tropas
 		btnPosicionar.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -104,7 +104,7 @@ public class PanelTabuleiro extends JPanel implements Observer {
 		});
 		add(btnPosicionar);
 
-		// Botão ataque
+		// BotÃ£o ataque
 		btnAtacar.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -117,7 +117,7 @@ public class PanelTabuleiro extends JPanel implements Observer {
 		});
 		add(btnAtacar);
 
-		// Botão deslocamento de tropas
+		// BotÃ£o deslocamento de tropas
 		btnDeslocar.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -131,7 +131,7 @@ public class PanelTabuleiro extends JPanel implements Observer {
 		});
 		add(btnDeslocar);
 
-		// Botão objetivo
+		// BotÃ£o objetivo
 		btnObjetivo.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -141,7 +141,7 @@ public class PanelTabuleiro extends JPanel implements Observer {
 		});
 		add(btnObjetivo);
 
-		// Botão próximo
+		// BotÃ£o prÃ³ximo
 		proximoLabel.setIcon(new ImageIcon("Imagens/war_btnProxJogada.png"));
 		proximoLabel.addMouseListener(new MouseAdapter() {
 			@Override
@@ -151,8 +151,17 @@ public class PanelTabuleiro extends JPanel implements Observer {
 			}
 		});
 		add(proximoLabel);
-
-		// Botão salvar
+		
+		// Botao cartas
+		btnCartas.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//TODO <==================================================== enfia o que abre a tela das cartas aqui >:)
+			}
+		});
+		add(btnCartas);
+		
+		// BotÃ£o salvar
 		btnSalvar.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -166,8 +175,12 @@ public class PanelTabuleiro extends JPanel implements Observer {
 		instanciaTerritoriosView();
 	}
 
+	
+	
+	
+	// comecam os desenhos na tela
     public void paintComponent(Graphics g) {
-        super.paintComponent(g);
+        super.paintComponent(g); 
         Graphics2D g2d=(Graphics2D) g;
         
         // Desenha as imagens de fundo
@@ -189,7 +202,7 @@ public class PanelTabuleiro extends JPanel implements Observer {
 		// Obtenha a cor do jogador
 		Color corJogador = stringToColor(jogadorAtual);
 
-		// Desenha um retângulo com a cor do jogador
+		// Desenha um retÃ¢ngulo com a cor do jogador
 		Rectangle2D retanguloJogador = new Rectangle2D.Double(1018, 210, 150, 120);
 		g2d.setColor(corJogador);
 		g2d.fill(retanguloJogador);
@@ -249,18 +262,21 @@ public class PanelTabuleiro extends JPanel implements Observer {
 				break;
 		}
 
-		// Posicionamento dos botões
+		// Posicionamento dos botÃµes
 		btnPosicionar.setBounds(1040, 120, 100, 30);
 		btnAtacar.setBounds(1040, 120, 100, 30);
 		btnDeslocar.setBounds(1040, 160, 100, 30);
 
 		btnObjetivo.setBounds(1040, 580, 100, 30);
+		
+		btnCartas.setBounds(1040, 500, 100, 30);
+		
 		btnSalvar.setBounds(1040, 500, 100, 30);
 		proximoLabel.setBounds(920, 580, 100, 50);
     }
 
 	public void notify(Observable o) { // O q e esse parametro ?
-		// Mudança nos territórios
+		// MudanÃ§a nos territÃ³rios
 		if (o.get() == null) {
 			int fase = Controller.getFase();
 			switch (fase) {
@@ -284,7 +300,7 @@ public class PanelTabuleiro extends JPanel implements Observer {
 			}
 		}
 		else {
-			// Mudança nos dados
+			// MudanÃ§a nos dados
 			int dados[] = (int[]) o.get();
 			DadoView.getDadoView().setDados(dados);
 			mostrarDados = true;
@@ -303,7 +319,7 @@ public class PanelTabuleiro extends JPanel implements Observer {
 			comboBoxRecebimentoContinente(possuiContinentes.get(0));
 			comboBoxExercContinente(possuiContinentes.get(0));
 		} else {
-			// Senão, recebe exercitos normalmente
+			// SenÃ£o, recebe exercitos normalmente
 			comboBoxRecebimento();
 			comboBoxExerc();
 		}
@@ -311,6 +327,7 @@ public class PanelTabuleiro extends JPanel implements Observer {
 		// Configura os componentes
 		cb3.setVisible(true);
 		btnPosicionar.setVisible(true);
+		btnCartas.setVisible(true);
 
 		cb2.setVisible(false);
 		btnAtacar.setVisible(false);
@@ -333,6 +350,7 @@ public class PanelTabuleiro extends JPanel implements Observer {
 		cb3.setVisible(false);
 		btnPosicionar.setVisible(false);
 		btnDeslocar.setVisible(false);
+		btnCartas.setVisible(false);
 		btnSalvar.setVisible(false);
 	}
 
@@ -345,9 +363,10 @@ public class PanelTabuleiro extends JPanel implements Observer {
 		btnDeslocar.setVisible(true);
 		proximoLabel.setVisible(true);
 		btnSalvar.setVisible(true);
-
+		
 		btnPosicionar.setVisible(false);
 		btnAtacar.setVisible(false);
+		btnCartas.setVisible(false);
 	}
 	
 	private void comboBoxRecebimento() {
@@ -585,18 +604,18 @@ public class PanelTabuleiro extends JPanel implements Observer {
 
 	// ANOTA O NOME DOS TERRITORIOS NO TABULEIRO
     private void instanciaTerritoriosView() {
-    	// América do Norte
+    	// AmÃ©rica do Norte
     	territorios.put("ALASCA", new TerritorioView(95,104,82,90,"ALASCA"));
     	territorios.put("CALGARY", new TerritorioView(190,110,170,94,"CALGARY"));
-    	territorios.put("GROENLÂNDIA", new TerritorioView(321,80,285,65,"GROENLÂNDIA"));
+    	territorios.put("GROENLÃ‚NDIA", new TerritorioView(321,80,285,65,"GROENLÃ‚NDIA"));
     	territorios.put("VANCOUVER", new TerritorioView(160,153,132,140,"VANCOUVER"));
     	territorios.put("QUEBEC", new TerritorioView(270,150,270,135,"QUEBEC"));
     	territorios.put("CALIFORNIA", new TerritorioView(106,230,80,213,"CALIFORNIA"));
     	territorios.put("TEXAS", new TerritorioView(175,200,170,185,"TEXAS"));
     	territorios.put("NOVA YORK", new TerritorioView(205,222,210,207,"NOVA YORK"));
-    	territorios.put("MÉXICO", new TerritorioView(150,315,120,298,"MEXICO"));
+    	territorios.put("MÃ‰XICO", new TerritorioView(150,315,120,298,"MEXICO"));
     	
-    	// América do Sul
+    	// AmÃ©rica do Sul
     	territorios.put("VENEZUELA", new TerritorioView(192,375,165,360,"VENEZUELA"));
     	territorios.put("PERU", new TerritorioView(230,425,218,407,"PERU"));
     	territorios.put("BRASIL", new TerritorioView(289,397,268,380,"BRASIL"));
@@ -605,47 +624,47 @@ public class PanelTabuleiro extends JPanel implements Observer {
     	// Oceania
     	territorios.put("PERTH", new TerritorioView(788,528,770,513,"PERTH"));
     	territorios.put("AUSTRALIA", new TerritorioView(844,551,820,535,"AUSTRALIA"));
-    	territorios.put("NOVA ZELÂNDIA", new TerritorioView(903,572,900,557,"NOVA ZELÂNDIA"));
-    	territorios.put("INDONÉSIA", new TerritorioView(841,422,860,430,"INDONÉSIA"));
+    	territorios.put("NOVA ZELÃ‚NDIA", new TerritorioView(903,572,900,557,"NOVA ZELÃ‚NDIA"));
+    	territorios.put("INDONÃ‰SIA", new TerritorioView(841,422,860,430,"INDONÃ‰SIA"));
     	
-    	// África
-    	territorios.put("ARGÉLIA", new TerritorioView(441,315,420,300,"ARGÉLIA"));
+    	// Ã�frica
+    	territorios.put("ARGÃ‰LIA", new TerritorioView(441,315,420,300,"ARGÃ‰LIA"));
     	territorios.put("EGITO", new TerritorioView(560,325,545,308,"EGITO"));
-    	territorios.put("NIGÉRIA", new TerritorioView(500,357,439,360,"NIGÉRIA"));
-    	territorios.put("SOMÁLIA", new TerritorioView(603,420,584,400,"SOMÁLIA"));
+    	territorios.put("NIGÃ‰RIA", new TerritorioView(500,357,439,360,"NIGÃ‰RIA"));
+    	territorios.put("SOMÃ�LIA", new TerritorioView(603,420,584,400,"SOMÃ�LIA"));
     	territorios.put("ANGOLA", new TerritorioView(544,443,525,425,"ANGOLA"));
-    	territorios.put("ÁFRICA DO SUL", new TerritorioView(565,495,530,477,"ÁFRICA DO SUL"));
+    	territorios.put("Ã�FRICA DO SUL", new TerritorioView(565,495,530,477,"Ã�FRICA DO SUL"));
     	
     	// Europa
     	territorios.put("REINO UNIDO", new TerritorioView(457,140,425,125,"REINO UNIDO"));
     	territorios.put("ESPANHA", new TerritorioView(432,233,410,216,"ESPANHA"));
-    	territorios.put("FRANÇA", new TerritorioView(480,195,460,178,"FRANÇA"));
-    	territorios.put("SUÉCIA", new TerritorioView(528,110,510,92,"SUÉCIA"));
-    	territorios.put("POLÔNIA", new TerritorioView(565,161,545,145,"POLÔNIA"));
-    	territorios.put("ITÁLIA", new TerritorioView(520,210,508,194,"ITÁLIA"));
-    	territorios.put("ROMÊNIA", new TerritorioView(573,214,555,236,"ROMÊNIA"));
-    	territorios.put("UCRÂNIA", new TerritorioView(590,185,587,205,"UCRÂNIA"));
+    	territorios.put("FRANÃ‡A", new TerritorioView(480,195,460,178,"FRANÃ‡A"));
+    	territorios.put("SUÃ‰CIA", new TerritorioView(528,110,510,92,"SUÃ‰CIA"));
+    	territorios.put("POLÃ”NIA", new TerritorioView(565,161,545,145,"POLÃ”NIA"));
+    	territorios.put("ITÃ�LIA", new TerritorioView(520,210,508,194,"ITÃ�LIA"));
+    	territorios.put("ROMÃŠNIA", new TerritorioView(573,214,555,236,"ROMÃŠNIA"));
+    	territorios.put("UCRÃ‚NIA", new TerritorioView(590,185,587,205,"UCRÃ‚NIA"));
     	
-    	// Ásia
-    	territorios.put("ESTÔNIA", new TerritorioView(670,106,650,91,"ESTÔNIA"));
-    	territorios.put("LETÔNIA", new TerritorioView(652,152,630,137,"LETÔNIA"));
+    	// Ã�sia
+    	territorios.put("ESTÃ”NIA", new TerritorioView(670,106,650,91,"ESTÃ”NIA"));
+    	territorios.put("LETÃ”NIA", new TerritorioView(652,152,630,137,"LETÃ”NIA"));
     	territorios.put("TURQUIA", new TerritorioView(710,203,686,187,"TURQUIA"));
-    	territorios.put("SÍRIA", new TerritorioView(670,234,628,238,"SÍRIA"));
-    	territorios.put("JORDÂNIA", new TerritorioView(611,290,585,275,"JORDÂNIA"));
-    	territorios.put("ARÁBIA SAUDITA", new TerritorioView(660,350,620,335,"ARÁBIA SAUDITA"));
+    	territorios.put("SÃ�RIA", new TerritorioView(670,234,628,238,"SÃ�RIA"));
+    	territorios.put("JORDÃ‚NIA", new TerritorioView(611,290,585,275,"JORDÃ‚NIA"));
+    	territorios.put("ARÃ�BIA SAUDITA", new TerritorioView(660,350,620,335,"ARÃ�BIA SAUDITA"));
     	territorios.put("IRAQUE", new TerritorioView(663,282,643,266,"IRAQUE"));
-    	territorios.put("IRÃ", new TerritorioView(705,291,695,276,"IRÃ"));
-    	territorios.put("PAQUISTÃO", new TerritorioView(725,257,695,242,"PAQUISTÃO"));
-    	territorios.put("RÚSSIA", new TerritorioView(765,117,748,100,"RÚSSIA"));
-    	territorios.put("SIBÉRIA", new TerritorioView(877,107,857,92,"SIBÉRIA"));
-    	territorios.put("CAZAQUISTÃO", new TerritorioView(785,168,799,172,"CAZAQUISTÃO"));
-    	territorios.put("MONGÓLIA", new TerritorioView(810,204,824,208,"MONGÓLIA"));
+    	territorios.put("IRÃƒ", new TerritorioView(705,291,695,276,"IRÃƒ"));
+    	territorios.put("PAQUISTÃƒO", new TerritorioView(725,257,695,242,"PAQUISTÃƒO"));
+    	territorios.put("RÃšSSIA", new TerritorioView(765,117,748,100,"RÃšSSIA"));
+    	territorios.put("SIBÃ‰RIA", new TerritorioView(877,107,857,92,"SIBÃ‰RIA"));
+    	territorios.put("CAZAQUISTÃƒO", new TerritorioView(785,168,799,172,"CAZAQUISTÃƒO"));
+    	territorios.put("MONGÃ“LIA", new TerritorioView(810,204,824,208,"MONGÃ“LIA"));
     	territorios.put("CHINA", new TerritorioView(780,245,765,230,"CHINA"));
     	territorios.put("COREIA DO NORTE", new TerritorioView(835,252,853,257,"COREIA DO NORTE"));
     	territorios.put("COREIA DO SUL", new TerritorioView(817,273,834,278,"COREIA DO SUL"));
-    	territorios.put("ÍNDIA", new TerritorioView(777,330,765,315,"ÍNDIA"));
+    	territorios.put("Ã�NDIA", new TerritorioView(777,330,765,315,"Ã�NDIA"));
     	territorios.put("BANGLADESH", new TerritorioView(829,314,794,299,"BANGLADESH"));
-    	territorios.put("TAILÂNDIA", new TerritorioView(883,304,895,309,"TAILÂNDIA"));
-    	territorios.put("JAPÃO", new TerritorioView(923,220,924,205,"JAPÃO"));
+    	territorios.put("TAILÃ‚NDIA", new TerritorioView(883,304,895,309,"TAILÃ‚NDIA"));
+    	territorios.put("JAPÃƒO", new TerritorioView(923,220,924,205,"JAPÃƒO"));
     }
 }
